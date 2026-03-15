9e12d03873bebcbd3bf3697dc6b4adbdde56d99b
Mohamed AbdulkadirAbdulahi <maxamedgoley@gmail.com>
2026-03-15 10:16:04 +0300

    update

---
+++ app/page.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Weather from './weather/page.jsx'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Weather/>
+    </div>
+  )
+}
+
+export default HomePage
+
+
+// import React from 'react'
+// import Portofolia from './portofolia/page.jsx'
+
+// const Porto = () => {
+//   return (
+//     <div>
+//       <Portofolia/>
+//     </div>
+//   )
+// }
+
+// export default Porto
+
+
+// import React from 'react'
+// import weather from './weather/page.jsx'
+
+
+// const HomePage = () => {
+//   return (
+//     <div>
+//       <weather/>
+//     </div>
+//   )
+// }
+
+// export default HomePage
--- app/weather/page.jsx
+++ app/weather/page.jsx
@@ -6,6 +6,7 @@ import { WiHumidity } from "react-icons/wi";
 import { BsStopwatch } from "react-icons/bs";
  import { CiCloud } from "react-icons/ci";
  import { IoSunnyOutline } from "react-icons/io5";
+import { FiCloudRain } from "react-icons/fi";
 
 
 
@@ -13,14 +14,14 @@ import { BsStopwatch } from "react-icons/bs";
 
 const Page = () => {
   return (
-    <div className='bg-blue-500 w-full min-h-screen lg:px-[20%] px-10 py-10'>
-      <div>
+    <div className='bg-blue-500 w-full min-h-screen lg:px-20 px-10 py-10'>
+      <div className='max-w-7xl mx-auto'>
         <div>
 <h1 className='text-4xl text-white font-bold'>Weather</h1>
-<p>Current conditions and forecast</p>
+<p className='text-gray-200'>Current conditions and forecast</p>
         </div>
-<div  className='mt-10 w-full lg:w-[40%]'>
-        <div className='bg-blue-300/40 px-10 py-5 rounded-lg'>
+<div  className='mt-10 w-full lg:flex lg:justify-between lg:items-start lg:gap-8'>
+        <div className='bg-blue-300/40 px-10 py-5 rounded-lg lg:w-[35%] w-full'>
           <div className='flex space-x-2 items-center text-gray-200'>
 <IoLocationOutline size={20}/>
 <p className='text-lg'>San Francisco, CA</p>
@@ -31,7 +32,7 @@ const Page = () => {
   <p className='text-lg text-gray-300'>H:24° L:18°</p>
 </div>
 
-<div className='grid grid-cols-2 gap-4 mt-10 lg:mt-20'>
+<div className='grid grid-cols-2 gap-4 mt-10'>
   <div className='bg-blue-300/40 p-2 rounded-lg'>
   <div className='flex space-x-2 text-gray-300 items-center'>
 <RiCloudWindyLine size={20}/>
@@ -71,41 +72,59 @@ const Page = () => {
 
        
 
-        <div className='bg-blue-300/40 mt-20 px-10 py-5 rounded-lg'>
+        <div className='bg-blue-300/40 mt-10 lg:mt-0 px-10 py-5 rounded-lg lg:flex-1 w-full min-w-0'>
           <h1 className='font-semibold text-xl'>Hourly Forecast</h1>
  
           <div className='flex gap-6 mt-5 overflow-x-auto pb-4'>
 
-          <div className=' text-xl flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
+          <div className=' text-sm flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
 <p >Now</p>
-<CiCloud  size={40}/>
+<CiCloud  size={25}/>
 <p>22°</p>
           </div>
 
-          <div className=' text-xl flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
-<p>11 AM</p>
-<IoSunnyOutline size={40} className="text-yellow-400"/>
+          <div className=' text-sm flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
+<p>11AM</p>
+<IoSunnyOutline size={25} className="text-yellow-400"/>
 <p>23°</p>
           </div>
 
-          <div className=' text-xl flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
-<p>12 PM</p>
-<IoSunnyOutline size={40} className="text-yellow-400"/>
+          <div className=' text-sm flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
+<p>12PM</p>
+<IoSunnyOutline size={25} className="text-yellow-400"/>
 <p>24°</p>
           </div>
 
-          <div className=' text-xl flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
-<p>1 PM</p>
-<IoSunnyOutline size={40} className="text-yellow-400"/>
+          <div className=' text-sm flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
+<p>1PM</p>
+<IoSunnyOutline size={25} className="text-yellow-400"/>
 <p>24°</p>
           </div>
 
-          <div className=' text-xl flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
-<p>2 PM</p>
-<CiCloud size={40}/>
+          <div className=' text-sm  flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
+<p>2PM</p>
+<CiCloud size={25}/>
 <p>23°</p>
           </div>
     
+
+  <div className=' text-sm flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
+<p>3PM</p>
+<FiCloudRain size={25} className="text-gray-400"/>
+<p>22°</p>
+          </div>
+
+            <div className=' text-sm flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
+<p>2PM</p>
+<CiCloud size={25} />
+<p>23°</p>
+          </div>
+
+     <div className=' text-sm flex flex-col gap-3 items-center bg-blue-300/20 py-5 px-4 rounded-lg'>
+<p>2PM</p>
+<CiCloud size={25}/>
+<p>23°</p>
+          </div>
 
           </div>
         </div>
